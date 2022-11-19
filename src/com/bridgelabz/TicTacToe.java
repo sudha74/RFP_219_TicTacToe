bbcdc69ff18c1e8321a0333b768625145e8a94b6
sudha123 <sudhanaukala1997@gmail.com>
2022-11-19 19:31:51 +0530

    Added:chooseLetter

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     static char[] board = new char[10];
+    static Scanner scr = new Scanner(System.in);
+    static char playerLetter;
+    static char computerLetter;
 
     static void createBoard(char[] board) {
 
@@ -9,8 +14,15 @@ public class TicTacToe {
             board[i] = ' ';
         }
     }
-
+    static void getLetter() {
+        System.out.println("Choose the letter(X or O)");
+        playerLetter = scr.next().toUpperCase().charAt(0);
+        computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+        System.out.println("Your Letter is " + playerLetter);
+        System.out.println("Computer Letter is " + computerLetter);
+    }
     public static void main(String[] args) {
         createBoard(board);
+        getLetter();
     }
 }
